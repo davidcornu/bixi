1f78454b40c53ca63f371c87aa96f4d5b690ce43
David Cornu <davidjcornu@gmail.com>
2012-08-01 22:56:14 -0400

    Improved parser to avoid skipping over valid matches and use a more robust parsing mechanism

--- parser.rb
+++ parser.rb
@@ -1,7 +1,7 @@
+# encoding=utf-8
+
 module Bixi
   class Parser
-    STATION_RXP = /var station = \{id:"(\d*)",name:"([\w|\-|\/|\s]*)",lat:"(\-?[\d|\.]*)",long:"(\-?[\d|\.]*)",nbBikes:"(\d*)",nbEmptyDocks:"(\d*)",installed:"(true|false)",locked:"(true|false)",temporary:"(true|false)"[\s|\w|,|:]*};/i
-
     def initialize(data)
       @data = data
     end
@@ -16,21 +16,32 @@ module Bixi
 
     private
 
+    STATION_RXP   = /var\s+station\s*=\s*{[^}]+}\s*;/x
+    ATTRIBUTE_RXP = /\s*(\w+)\s*:\s*"([^"]+)"/x
+    VALID_ATTRIBUTES = %w{id name lat long nbBikes nbEmptyDocks installed locked temporary}
+
+    def cast(string)
+      if string.match /\A\d+\z/
+        string.to_i
+      elsif string.match /\A-?\d+\.\d+\z/
+        string.to_f
+      elsif ['true', 'false'].include?(string)
+        string == 'true'
+      else
+        string.gsub(/\\'/, "'")
+      end
+    end
+
     def station_matches
-      matches = @data.scan STATION_RXP
-      matches ||= []
-      matches.map do |match|
-        {
-          :id           => match[0].to_i,
-          :name         => match[1],
-          :lat          => match[2].to_f,
-          :long         => match[3].to_f,
-          :nbBikes      => match[4].to_i,
-          :nbEmptyDocks => match[5].to_i,
-          :installed    => match[6] == 'true',
-          :locked       => match[7] == 'true',
-          :temporary    => match[8] == 'true'
-        }
+      raw_stations = @data.scan STATION_RXP
+      raw_stations.map do |raw_station|
+        station = {}
+        raw_station.scan(ATTRIBUTE_RXP).each do |match|
+          if match.size == 2
+            station[match[0]] = cast(match[1])
+          end
+        end
+        station.select {|k,v| VALID_ATTRIBUTES.include?(k)}
       end
     end
   end
