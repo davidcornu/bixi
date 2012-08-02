99370aea91441b3c9ded24ac832fa31dbcfc1e25
David Cornu <davidjcornu@gmail.com>
2012-08-01 22:55:00 -0400

    Added better error handling, moved to class structure and added multiple city support

--- app.rb
+++ app.rb
@@ -1,19 +1,53 @@
-APP_DIR = File.expand_path File.dirname(__FILE__)
+# encoding=utf-8
 
 require 'rubygems'
 require 'bundler'
 require 'open-uri'
 Bundler.require
 
+APP_DIR = File.expand_path File.dirname(__FILE__)
 ['parser.rb', 'cache.rb'].each {|file| require File.join(APP_DIR, file) }
 
-def fetch_json
-  data = open('https://montreal.bixi.com/maps/statajax').read
-  parser = Bixi::Parser.new(data)
-  JSON.dump parser.stations
-end
+module Bixi
+  class App < Sinatra::Base
+    VALID_CITIES = ['montreal', 'toronto', 'ottawa']
 
-get '/' do
-  content_type :json
-  Bixi::Cache[:stations] ||= fetch_json
+    before do
+      content_type :json
+    end
+
+    get '/:city' do
+      if VALID_CITIES.include?(params[:city])
+        Bixi::Cache[params[:city]] ||= fetch_json(params[:city])
+      else
+        pass
+      end
+    end
+
+    get '*' do
+      status 404
+      JSON.dump :supported_paths => ['/montreal', '/toronto', '/ottawa']
+    end
+
+    private
+
+    def bixi_url(city)
+      subdomains = {
+        'montreal' => 'montreal',
+        'toronto'  => 'toronto',
+        'ottawa'   => 'capitale'
+      }
+      return "https://#{subdomains[city]}.bixi.com/maps/statajax"
+    end
+
+    def fetch_json(city)
+      begin
+        data = open(bixi_url(city)).read
+        parser = Bixi::Parser.new(data)
+        JSON.dump parser.stations
+      rescue OpenURI::HTTPError
+        JSON.dump :error => "Data not currently available for #{city.capitalize}"
+      end
+    end
+  end
 end
